77523cd37ef4e815a353ef24f702665285daaace
sumantrai-ok <sumantraiok@gmail.com>
2025-07-23 13:48:20 +0000

    datatypes in javascript

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"  // treat all js code as newer version
+
+// alert(3 + 3)   we are using nodejs not browser
+
+let name = "Sumant"
+let age = 20
+let isLoggedIn = true
+let state = null
+
+
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null  => standalon value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null);        // object
+
+
+
+
