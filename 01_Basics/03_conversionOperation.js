65cd9945a4369955b50f46e06819ea21e2dd2e9a
sumantrai-ok <sumantraiok@gmail.com>
2025-07-23 14:10:04 +0000

    conversions of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "sumant"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "sumant"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "sumant" => true
+
+let someNumber = 33
+
+let strNumber = String(someNumber)
+console.log(strNumber);
+console.log(typeof strNumber);
