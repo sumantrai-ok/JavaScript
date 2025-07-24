f25402e46f0bbc31a3fc245fd59944a56291346a
sumantrai-ok <sumantraiok@gmail.com>
2025-07-24 09:15:37 +0000

    json in javascript

--- 02_Bascis/04_object.js
+++ 02_Bascis/04_object.js
@@ -63,11 +63,44 @@ const users = [
 
 users[1].email
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "sumant"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+
+// const navbar = ({company}) => {            // React
+                                     
+// }
+
+// navbar(company = "sumant")
+
+
+
+// {
+//    " name": "sumant",
+//     "coursename": "js in hindi",
+//    " price": "free"
+// }
+
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
