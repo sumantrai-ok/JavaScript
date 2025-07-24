487f545bd1bcd2e0f02786f90b6fed6701e4a8e4
sumantrai-ok <sumantraiok@gmail.com>
2025-07-24 11:19:18 +0000

    function in js

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -38,4 +38,38 @@ function loginUserMessage(username = "sam"){
 // console.log(message);
 
 // console.log(loginUserMessage("Shubhangi"));
-console.log(loginUserMessage("sumant"));
+// console.log(loginUserMessage("sumant"));
+
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "rai",
+    price: 199
+}
+
+function handleOject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price} `);
+    
+}
+
+// handleOject(user)
+
+handleOject({
+    username: "Shubhangi",
+    price:299
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
